e02d1b153542032e075c4d28ba5f20665365cbbc
juwoon01 <juwoon01@naver.com>
2020-07-14 17:44:38 +0900

    PS-2020-07-14 change to for()

--- 2020-07/BO2292.cpp
+++ 2020-07/BO2292.cpp
@@ -5,10 +5,16 @@ int main(void){
   int num;
   int count=1;
   int range=1;
-  int plus=0;
   cin >> num;
 
-    while (true) {
+  for(count=1;range<num;count++){
+    range += count*6;
+  }
+
+  cout << count;
+
+//  int plus=0;
+  /*  while (true) {
       if(num>range){
         plus+=6;
         range+=plus;
@@ -18,7 +24,9 @@ int main(void){
         cout<<count;
         break;
       }
-    }
+    }*/
+
+
 
 
 
