3b010cd1a5389c2f732e61e0a4c2488b083ec4a2
juwoon01 <juwoon01@naver.com>
2020-07-16 18:19:19 +0900

    PS-2020-07-16

---
+++ 2020-07/BOJ2869.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace::std;
+
+int main(void){
+  int a;
+  int b;
+  int v;
+  int answer=0;
+  cin >> a >> b >> v;
+
+  if((v-a)%(a-b)==0){
+    cout << (v-a)/(a-b) + 1;
+  }
+  else{
+    cout << (v-a)/(a-b) + 2;
+  }
+  return 0;
+}
