30a95b4b45cf11a79e2b9ed687ff08195740b08a
juwoon01 <juwoon01@naver.com>
2020-07-20 18:00:51 +0900

    PS 2020-07-20

--- 2020-07/BOJ1011.cpp
+++ 2020-07/BOJ1011.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
-
+#include <cmath>
 using namespace::std;
 
 int main(void){
-  int t, k, n;
+  int t;
+  long long x, y;
+  int count;
+  cin >> t;
 
+  for(int i=0;i<t;i++){
+    cin >> x >> y;
+    count=0;
+    while(count*count<(y-x)){
+      count++;
+    }
+    long long rem = (y-x) - count*count;
+    rem = (long long)ceil((double)rem/(double)count);
+    cout << (2*count)-1+rem << '\n';
+  }
 
 
   return 0;
