df1151eed0896c7da89f1f9179896d2aa85bd36e
juwoon01 <juwoon01@naver.com>
2020-07-23 17:27:31 +0900

    PS 2020-07-23

--- 2020-07/BOJ4948.cpp
+++ 2020-07/BOJ4948.cpp
@@ -2,27 +2,35 @@
 
 using namespace::std;
 
-int prime(int n){
-  for(int i=2;i<n;i++){
-    if(n%i==0){
-      return 0;
-    }
-  }
-  return 1;
-}
-
 int main(void){
-  int n = 1;
-  while(n != 0){
-    int count = 0;
+  int n;
+  int max = 246912;
+  bool arr[246913];
+  for(int i=2; i<=max; i++){
+    arr[i]=true;
+  }
+
+    for(int i=2; i*i<=max; i++){
+      if(arr[i]){
+        for(int j=i*i;j<=max;j+=i){
+          arr[j]=false;
+        }
+      }
+    }
+  while(true){
     cin >> n;
-    for(int i=n+1; i<=n*2;i++){
-      if(prime(i)){
+    if(n==0){
+      break;
+    }
+    int count = 0;
+
+    for(int i=n+1; i<=2*n;i++){
+      if(arr[i]){
         count++;
       }
     }
 
-    cout << count << '\n';
+    cout << count << endl;
   }
 
   return 0;
