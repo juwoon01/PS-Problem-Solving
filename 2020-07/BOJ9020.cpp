a68b056bf09db2f1b2a6e6d0fe52d3e3c0eed9fc
juwoon01 <juwoon01@naver.com>
2020-07-25 20:10:26 +0900

    PS 2020-07-25

--- 2020-07/BOJ9020.cpp
+++ 2020-07/BOJ9020.cpp
@@ -4,8 +4,36 @@ using namespace::std;
 
 int main(void){
   int t;
-  cin << t;
+  cin >> t;
+  int n;
+
+  int max = 10000;
+  bool arr[10001];
+
+  for(int i=2; i<=max; i++){
+    arr[i]=true;
+  }
+
+    for(int i=2; i*i<=max; i++){
+      if(arr[i]){
+        for(int j=i*i;j<=max;j+=i){
+          arr[j]=false;
+        }
+      }
+    }
+
+
+
   for(int i=0;i<t;i++){
+    cin >> n;
+    int mid=n/2;
+    int j,k;
+    for(j=k=mid;;j--, k++){
+      if(arr[j] && arr[k]){
+          cout << j <<' ' << k << endl;
+          break;
+      }
+    }
 
   }
 
