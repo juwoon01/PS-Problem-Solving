df0961999f155362fe7345af81d4eff3a59422d4
juwoon01 <juwoon01@naver.com>
2020-07-28 15:40:38 +0900

    PS 2020-07-28

---
+++ 2020-07/BOJ3053.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+using namespace::std;
+
+int main(void){
+  float r;
+  scanf("%f", &r);
+  printf("%.6f \n",3.14159265358979323846*r*r);
+  printf("%.6f",2*r*r);
+  return 0;
+}
