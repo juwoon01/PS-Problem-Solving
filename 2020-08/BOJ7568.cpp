2b96df061c16420e8ca25ac086f2c0fef3c7f75f
juwoon01 <juwoon01@naver.com>
2020-08-07 22:53:52 +0900

    PS 2020-08-07

--- 2020-08/BOJ7568.cpp
+++ 2020-08/BOJ7568.cpp
@@ -4,8 +4,27 @@ using namespace::std;
 int main(void){
   int n;
   cin >> n;
+  int arr[n][3];
   for(int i=0;i<n;i++){
-
+    cin >> arr[i][0] >> arr[i][1];
+    arr[i][2]=1;
   }
+
+  for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+      if(arr[i][0]>arr[j][0] && arr[i][1]>arr[j][1] ){
+        arr[j][2]++;
+      }
+      else if(arr[i][0]<arr[j][0] && arr[i][1]<arr[j][1]){
+        arr[i][2]++;
+      }
+    }
+  }
+
+  for(int i=0;i<n;i++){
+    cout << arr[i][2] << ' ';
+  }
+
+
   return 0;
 }
