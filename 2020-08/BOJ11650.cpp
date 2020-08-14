632ad8405726ccb6985290802454112634b036ae
juwoon01 <juwoon01@naver.com>
2020-08-14 17:23:10 +0900

    PS 2020-08-14 time out

---
+++ 2020-08/BOJ11650.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace::std;
+
+int main(void){
+  int n;
+  cin >> n;
+  int xy[n][2];
+  for(int i=0;i<n;i++){
+    cin >> xy[i][0] >> xy[i][1];
+  }
+  for(int i=0;i<n-1;i++){
+    for(int j=0;j<n-i-1;j++){
+      if(xy[i][0]==xy[i+1][0]){
+        if(xy[i][1]>xy[i+1][1]){
+          int tmp[2];
+          tmp[0] = xy[i][0];
+          tmp[1] = xy[i][1];
+          xy[i][0] = xy[i+1][0];
+          xy[i][1] = xy[i+1][1];
+          xy[i+1][0]=tmp[0];
+          xy[i+1][1]=tmp[1];
+        }
+      }
+      else if(xy[i][0]>xy[i+1][0]){
+        int tmp[2];
+        tmp[0] = xy[i][0];
+        tmp[1] = xy[i][1];
+        xy[i][0] = xy[i+1][0];
+        xy[i][1] = xy[i+1][1];
+        xy[i+1][0]=tmp[0];
+        xy[i+1][1]=tmp[1];
+      }
+
+    }
+  }
+
+  for(int i=0;i<n;i++){
+    cout << xy[i][0]<< ' ' << xy[i][1] << '\n';
+  }
+  return 0;
+}
