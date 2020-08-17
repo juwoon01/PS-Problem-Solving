3659fe3e07432e479a7deffa10d7c3c5ad5b3419
juwoon01 <juwoon01@naver.com>
2020-08-17 21:16:28 +0900

    PS 2020-08-17

--- 2020-08/BOJ15649.cpp
+++ 2020-08/BOJ15649.cpp
@@ -2,10 +2,22 @@
 
 
 using namespace::std;
+int n,m;
+int check[9];
+int stack[9];
+void dfs(int count){
+
+
+  }
+}
 
 int main(void){
-  int n;
-  cin >> n;
+
+  cin >> n >> m;
+
+  for(int i=1;i<=n;i++){
+    dfs(i);
+  }
 
 
   return 0;
