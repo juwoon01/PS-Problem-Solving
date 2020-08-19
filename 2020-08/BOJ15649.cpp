4ca129bc507bec80fb23a694eef61f369be46213
juwoon01 <juwoon01@naver.com>
2020-08-19 23:23:10 +0900

    PS 2020-08-19

--- 2020-08/BOJ15649.cpp
+++ 2020-08/BOJ15649.cpp
@@ -4,10 +4,22 @@
 using namespace::std;
 int n,m;
 int check[9];
-int stack[9];
+int stack[8];
 void dfs(int count){
-
-
+  if(count==m){
+    for(int i=0;i<m;i++){
+      cout << stack[i] << " ";
+    }
+    cout << "\n";
+    return;
+  }
+  for(int i=1;i<=n;i++){
+    if (!check[i]) {
+              check[i] = true;
+              stack[count] = i;
+              dfs(count+1);
+              check[i] = false;
+          }
   }
 }
 
@@ -15,7 +27,7 @@ int main(void){
 
   cin >> n >> m;
 
-  for(int i=1;i<=n;i++){
+  for(int i=0;i<n;i++){
     dfs(i);
   }
 
