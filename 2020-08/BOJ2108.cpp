ac45e0f3ca2c3cbe51ace03345e0c088aa309415
juwoon01 <juwoon01@naver.com>
2020-08-21 16:47:02 +0900

    PS 2020-08-21 TIME OUT

--- 2020-08/BOJ2108.cpp
+++ 2020-08/BOJ2108.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace::std;
 
 int main(void){
@@ -6,24 +7,29 @@ int main(void){
   cin >> n;
   int arr[n];
   int answer[4];
-  int min=4444;
-  int max=-4444;
-  for(int i=0;i<n;i++){
-    cin >> arr[i];
-  }
+
+
   answer[0]=0;
 
   for(int i=0;i<n;i++){
-
+    cin >> arr[i];
     answer[0] += arr[i];
+  }
 
-    if(min > arr[i]){
-      min = arr[i];
-    }
-    if(max < arr[i]){
-      max = arr[i];
+  for(int i=0;i<n;i++){
+
+    for(int j=0;j<n-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        int tmp;
+        tmp=arr[j+1];
+        arr[j+1]=arr[j];
+        arr[j]=tmp;
+      }
     }
   }
+  int min=arr[0];
+  int max=arr[n-1];
+
 
   int count=0;
   int freq[2]={4444,4444};
@@ -62,9 +68,7 @@ int main(void){
 
   }
 
-
-
-  answer[0] = (answer[0]/n) + 0.5;
+  answer[0] = round((double)answer[0]/n);
   answer[1] = arr[n/2];
   answer[2] = freq[0];
   answer[3] = max - min;
