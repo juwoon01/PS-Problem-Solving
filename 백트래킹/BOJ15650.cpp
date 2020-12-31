eb8ce601996ab617136f5e226c931a754635c8cd
juwoon01 <juwoon01@naver.com>
2020-12-31 22:13:36 +0900

    PS 2020-12-31

--- 백트래킹/BOJ15650.cpp
+++ 백트래킹/BOJ15650.cpp
@@ -5,12 +5,13 @@
   첫째 줄에 자연수 N과 M이 주어진다. (1 ≤ M ≤ N ≤ 8)
   한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다.
   중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
-  수열은 사전 순으로 증가하는 순서로 출력해야 한다.
+  1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+  고른 수열은 오름차순이어야 한다.
 
 풀이
-  1. 4 2 시 12 13 14 21 23 24....  3 3 시 123 132 213 231 312 321
+  1. 4 2 시 12 13 14 23 24 34
   2. 중복을 허용하는 수열 작성
-  3. 중복 제거 (check 배열 사용)
+  3. 루프 노드와 단말 노드로 나누어 중복 제거
 */
 
 
@@ -27,26 +28,34 @@ void print(){
   cout << '\n';
 }
 
-void tree(int index){
+void leaf(int index, int num){
   if(index == M ){
     print();
     return;
   }
-  for(int i=1; i<=N; i++){
+  for(int i=num; i<=N; i++){
     if(!check[i]){//넣으려는 숫자가 false면(없으면)
       stack[index] = i;
       check[i] = true;
-      tree(index+1);
+      leaf(index+1, i+1);
       check[i] = false;
     }
 
-    }
+  }
+}
+
+void roof(int index){
+  for(int i=1; i<=N; i++){
+      stack[index] = i;
+      check[i] = true;
+      leaf(index+1,i+1);
+  }
 }
 
 int main(void){
 
   cin >> N >> M;
-  tree(0);
+  roof(0);
 
   return 0;
 }
