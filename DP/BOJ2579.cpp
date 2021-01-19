74f354cf57fb3ee120778d8ba7cbe321cb1ec5c0
juwoon01 <juwoon01@naver.com>
2021-01-19 12:30:07 +0900

    PS 2021-01-19

--- DP/BOJ2579.cpp
+++ DP/BOJ2579.cpp
@@ -1,4 +1,4 @@
-/*https://www.acmicpc.net/problem/1932*/
+/*https://www.acmicpc.net/problem/2579*/
 /*
 동적계획법
   같이 각각의 계단에는 일정한 점수가 쓰여 있는데 계단을 밟으면 그 계단에 쓰여 있는 점수를 얻게 된다.
@@ -10,16 +10,22 @@
 
 풀이
   1. count를 통해 1일때 건너뛰기, 2일때 건너뛰기 재귀
-  1.1 마지막을 밟지 못하면 제외, 최대값을 구함
+    1.1 마지막을 밟지 못하면 제외, 최대값을 구함 -> 시간초과
+  2. 동적계획법 사용
+    2.1 점화식: 마지막 계단 + 이전 계단 + 전전전 계단
+               마지막 계단 + 전전 계단
+               1번 점화식 후 2번 점화식 가능
+               2번 점화식 후 1번 점화식 가능 -> 모든 경우 연산 가능
+
 */
 
 #include <iostream>
 using namespace::std;
-
+/*
 int score[300];
 int MAX = -1;
 int n;
-
+ //재귀만으로(시간초과)
 void recu(int count, int index,int sum){
   if(index == n){
     if(sum > MAX){
@@ -42,21 +48,42 @@ void recu(int count, int index,int sum){
     count = 0;
     recu(count,index+2,sum);//다음칸으로
   }
-
-
-
-
 }
-
+*/
+int checkMax(int num1, int num2){
+  return (num1 > num2) ? num1 : num2;
+}
 int main(void){
+  int MAX = -1;
+  int dp[300];
+  int score[300];
+  int n;
 
   cin >> n;
   for(int i=0;i<n;i++){
     cin >> score[i];
   }
 
-  recu(0,0,0);
+  if(n == 1){
+    cout << score[0];
+    return 0;
+  }
+  else if( n == 2){
+    cout << score[0] + score[1];
+    return 0;
+  }
+  else if(n == 3){
+    cout << checkMax(score[1] + score[2], score[0] + score[2]);
+    return 0;
+  }
 
-  cout << MAX;
+  dp[0] = score[0];
+  dp[1] = score[0] + score[1];
+  dp[2] = checkMax(score[1] + score[2], score[0] + score[2]);
+
+  for(int i=3;i<n;i++){
+    dp[i] = checkMax(score[i] + score[i-1] + dp[i-3], score[i] + dp[i-2]);
+  }
+  cout << dp[n-1];
   return 0;
 }
