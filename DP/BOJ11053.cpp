10c452b6871c88e71f8d71847e71e1112505e143
juwoon01 <juwoon01@naver.com>
2021-01-25 10:18:53 +0900

    PS 2021-01-25

--- DP/BOJ11053.cpp
+++ DP/BOJ11053.cpp
@@ -15,7 +15,11 @@
 첫째 줄에 수열 A의 가장 긴 증가하는 부분 수열의 길이를 출력한다.
 
 풀이
-  1.1 전부 돌려본다.
+  1. 전체를 돌린다 -> 10 20 30 1 2 3 100 10 12 -> (1 ,2, 3, 100) 원하는 답: (1, 2, 3, 10, 12)
+  1. 각 순간의 최대값을 dp로 저장 -> 값보다 크면 +1
+
+
+
 */
 
 #include <iostream>
@@ -24,30 +28,29 @@ using namespace::std;
 
 int main(void){
   int A[1000];
+  int dp[1000];
   int N;
-
+  int max = 1;
   cin >> N;
   for(int i=0;i<N;i++){
     cin >> A[i];
   }
+  dp[0] = 1;
 
-  int result = 0;
-  for(int i=0; i<N-1; i++){
-    int MAX = A[i];
-    int count = 1;
-    for(int j=i+1; j<N; j++){
-      if(A[j] > MAX){
-        MAX = A[j];
-        count++;
+  for(int i=1;i<N;i++){
+    int now = 0;
+    for(int j=0;j<i;j++){
+      if(A[i] > A[j]){
+        now = dp[j] > now ? dp[j] : now;
       }
     }
-
-    if(count > result){
-      result = count;
+    dp[i] = now+1;
+    if(dp[i] > max){
+      max = dp[i];
     }
   }
 
-  cout << result;
+  cout << max;
 
   return 0;
 }
